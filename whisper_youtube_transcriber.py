57cbe50dfc7462b2a9ebbcdf7016b5e9531a1a8f
vlaminckaxel <axel@datameister.ai>
2024-03-24 11:38:03 +0100

    update code

--- whisper_youtube_transcriber.py
+++ whisper_youtube_transcriber.py
@@ -7,23 +7,33 @@ import pathlib
 
 logger = logging.getLogger(__name__)
 
-def transcribe_video(youtube_url: str, output_dir: pathlib.Path):
+
+def download_audio(youtube_url: str, output_dir: pathlib.Path, force: bool = False):
     # Ensure output directory exists
     output_dir.mkdir(exist_ok=True)
 
     # Download video
-    logging.info(f"Downloading video from {youtube_url}")
     yt = YouTube(youtube_url)
     youtube_video = yt.streams.filter(progressive=True, file_extension='mp4').order_by(
         'resolution'
     ).desc().first()
-    video_file = pathlib.Path(youtube_video.download(output_dir))
+    video_file = output_dir / f"{yt.title}.mp4"
+
+    if not video_file.exists() or force:
+        logging.info(f"Downloading video from {youtube_url}")
+        youtube_video.download(output_dir)
 
     # Convert video to audio
-    logging.info(f"Converting video to audio")
     audio_file = video_file.with_suffix('.mp3')
-    video = mp.VideoFileClip(str(video_file))
-    video.audio.write_audiofile(str(audio_file))
+    if not audio_file.exists() or force:
+        logging.info(f"Converting video to audio")
+        video = mp.VideoFileClip(str(video_file))
+        video.audio.write_audiofile(str(audio_file))
+    return audio_file
+
+def transcribe_video(youtube_url: str, output_dir: pathlib.Path):
+    # Get audio from YouTube
+    audio_file = download_audio(youtube_url, output_dir)
 
     # Transcribe audio
     logging.info(f"Transcribing audio")
